81e3bd61ab37f7cbc2e7d66931b0605a19947150
rasuloff789 <rasuloff789@gmail.com>
2020-12-17 23:46:22 +0500

    data added

---
+++ src/data.js
@@ -0,0 +1,55 @@
+let Informations = [
+  {
+    lang : "en",
+    langExtention : `English`,
+    passwordText : `Success` ,
+    inputPassword : `Password`,
+    inputLogin : `Login`,
+    movieWhoAmIInfo : `Benjamin is a young computer genius. Since childhood, she dreams of becoming a superhero from comics and conquers the world. But in the real world, he is Nobody. His life suddenly changes when he meets his complete opposite - the flow of service guy Max.`, 
+    aboutInformation : `Information can be thought of as the resolution of uncertainty; it is that which answers the question of "What an entity is" and thus defines both its essence and nature of its characteristics. The concept of information has different meanings in different contexts.[1] Thus the concept becomes related to notions of constraint, communication, control, data, form, education, knowledge, meaning, understanding, mental stimuli, pattern, perception, representation, and entropy.
+    
+    Information is associated with data, as data represent values attributed to parameters, and information is data in context and with meaning attached. Information also relates to knowledge, as knowledge signifies understanding of an abstract or concrete concept.[2][better source needed]
+    
+    In terms of communication, information is expressed either as the content of a message or through direct or indirect observation. That which is perceived can be construed as a message in its own right, and in that sense, information is always conveyed as the content of a message.
+    
+    Information can be encoded into various forms for transmission and interpretation (for example, information may be encoded into a sequence of signs, or transmitted via a signal). It can also be encrypted for safe storage and communication.
+    
+    The uncertainty of an event is measured by its probability of occurrence and is inversely proportional to that. The more uncertain an event, the more information is required to resolve uncertainty of that event. The bit is a typical unit of information, but other units such as the nat may be used. For example, the information encoded in one "fair" coin flip is log2(2/1) = 1 bit, and in two fair coin flips is log2(4/1) = 2 bits.`
+  },
+  {
+    lang : "ru",
+    langExtention : `Pусский`,
+    passwordText : `Успех` ,
+    inputPassword : `пароль`,
+    inputLogin : `Aвторизоваться`,
+    movieWhoAmIInfo : `Бенджамин — молодой компьютерный гений. С детства он мечтает стать супергероем из комиксов и покорить мир. Но в реальном мире он — Никто. Его жизнь неожиданно меняется, когда он встречает свою полную противоположность — харизматичного парня Макса.`, 
+    aboutInformation : `Информацию можно рассматривать как разрешение неопределенности; это то, что отвечает на вопрос «Что такое сущность» и, таким образом, определяет как ее сущность, так и природу ее характеристик. Концепция информации имеет разное значение в разных контекстах. [1] Таким образом, концепция становится связанной с понятиями ограничения, коммуникации, контроля, данных, формы, образования, знания, значения, понимания, ментальных стимулов, паттерна, восприятия, представления и энтропии.
+    
+    Информация связана с данными, поскольку данные представляют собой значения, приписываемые параметрам, а информация - это данные в контексте и со смыслом. Информация также относится к знаниям, поскольку знание означает понимание абстрактного или конкретного понятия. [2] [необходим лучший источник]
+    
+    С точки зрения коммуникации информация выражается либо в виде содержания сообщения, либо посредством прямого или косвенного наблюдения. То, что воспринимается, может быть истолковано как сообщение само по себе, и в этом смысле информация всегда передается как содержание сообщения.
+    
+    Информация может быть закодирована в различных формах для передачи и интерпретации (например, информация может быть закодирована в последовательность знаков или передана через сигнал). Он также может быть зашифрован для безопасного хранения и связи.
+    
+    Неопределенность события измеряется вероятностью его наступления и обратно пропорциональна ей. Чем более неопределенным является событие, тем больше информации требуется для разрешения неопределенности этого события. Бит - это типичная единица информации, но могут использоваться и другие единицы, такие как nat. Например, информация, закодированная в одном "честном" подбрасывании монеты, имеет вид log2 (2/1) = 1 бит, а в двух "честных" подбрасываниях монеты - log2 (4/1) = 2 бита.`
+  },
+  {
+    lang : "uz",
+    langExtention : `O'zbek`,
+    passwordText : `Muvaffaqiyat` ,
+    inputPassword : `Parol`,
+    inputLogin : `Login`,
+    movieWhoAmIInfo : `Benjamin yosh kompyuter dahosi. Bolaligidan u komikslar superqahramoni bo'lishni va dunyoni zabt etishni orzu qilardi. Ammo haqiqiy dunyoda u Hech kim emas. Uning hayoti uning aksincha - xarizmatik erkak do'sti Maks bilan uchrashganda to'satdan o'zgaradi.`, 
+    aboutInformation : `Axborotni noaniqlikning echimi sifatida ko'rish mumkin; aynan shu narsa "mavjudlik nima" degan savolga javob beradi va shu bilan uning mohiyatini ham, xususiyatlarining xususiyatini ham belgilaydi. Axborot tushunchasi har xil sharoitda har xil ma'noga ega. [1] Shunday qilib, kontseptsiya cheklash, aloqa, nazorat, ma'lumotlar, shakl, ta'lim, bilim, ma'no, tushunish, aqliy rag'batlantirish, naqsh, idrok, vakillik va entropiya tushunchalari bilan bog'liq bo'ladi.
+    
+    Axborot ma'lumotlar bilan bog'liq, chunki ma'lumotlar parametrlarga berilgan qiymatlar, ma'lumotlar esa kontekst va ma'noga ega ma'lumotlardir. Axborot ham bilimga ishora qiladi, chunki bilim mavhum yoki aniq tushunchani tushunishni anglatadi. [2] [yaxshiroq manbaga ehtiyoj bor]
+    
+    Aloqa nuqtai nazaridan ma'lumot yoki xabarning mazmuni shaklida yoki to'g'ridan-to'g'ri yoki bilvosita kuzatuv orqali ifodalanadi. Qabul qilinadigan narsa o'z-o'zidan xabar sifatida talqin qilinishi mumkin va shu ma'noda ma'lumot har doim xabarning mazmuni sifatida etkaziladi.
+    
+    Axborot uzatish va talqin qilish uchun turli shakllarda kodlanishi mumkin (masalan, ma'lumotlar belgilar ketma-ketligiga kodlanishi yoki signal orqali uzatilishi mumkin). Bundan tashqari, xavfsiz saqlash va aloqa uchun shifrlangan bo'lishi mumkin.
+    
+    Hodisaning noaniqligi uning yuzaga kelish ehtimoli bilan o'lchanadi va unga teskari proportsionaldir. Hodisa qanchalik noaniq bo'lsa, ushbu hodisaning noaniqligini hal qilish uchun ko'proq ma'lumot talab qilinadi. Bit - bu ma'lumotlarning odatiy birligi, ammo nat kabi boshqa birliklardan foydalanish mumkin. Masalan, bitta "adolatli" tanga tashlashda kodlangan ma'lumot log2 (2/1) = 1 bit, ikkita "adolatli" tanga tepasida log2 (4/1) = 2 bit.`
+  }
+];
+
+export default Informations;
