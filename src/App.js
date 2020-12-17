addc4219f3c4d8a139a990ca1725cf1580f8f026
rasuloff789 <rasuloff789@gmail.com>
2020-12-17 23:46:15 +0500

    app end

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Header from "../src/components/Header";
+import Form from "../src/components/Form";
+import Footer from "../src/components/Footer";
 
 function App() {
   return (
     <>
     <div>
-    
+    <Header />
+    <Form />
+    <Footer />
     </div>
     </>
     );
